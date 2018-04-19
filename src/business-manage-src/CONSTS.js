13518dc765cb50d40ea222b769824a329d4a0c6a
emsigant <emsigant@qq.com>
2018-04-19 10:48:35 +0800

    add commas

--- src/business-manage-src/CONSTS.js
+++ src/business-manage-src/CONSTS.js
@@ -6,7 +6,7 @@ export const STATUS_CODE = {
     PENDING: 'pending',
     NOT_EXIST: 'not-exist',
     NOT_LOGIN: 'not-login',
-    PSW_WRONG: 'psw-wrong'
+    PSW_WRONG: 'psw-wrong',
 };
 
 // 所有API常量
@@ -24,8 +24,8 @@ export const APIS = {
 export const COMMON_FETCH_OPTIONS = {
     credentials: 'include',
     headers: {
-        'Content-Type': 'application/json'
-    }
+        'Content-Type': 'application/json',
+    },
 };
 
 // actions consts
@@ -35,10 +35,10 @@ export const ACTIONS_CONSTS = {
         CER_DATA_STATUS_CHANGE: '2',
         CER_SUBMIT: '3',
         CER_SUBMIT_STATUS_CHANGE: '4',
-        CER_CLEAR: '5'
+        CER_CLEAR: '5',
     },
     ACCOUNT: {
-        ACCOUNT_SUBMIT_STATUS_CHANGE: '6'
+        ACCOUNT_SUBMIT_STATUS_CHANGE: '6',
     },
     ORDER: {
         // order-data
@@ -62,14 +62,14 @@ export const FAIL_INFOS = {
     FETCH: '获取信息失败，请重试',
     SUBMIT: '提交失败，请重试',
     UPDATE: '更新失败，请重试',
-    DELETE: '删除失败，请重试'
+    DELETE: '删除失败，请重试',
 };
 
 export const SUCC_INFOS = {
     FETCH: '成功获取信息',
     SUBMIT: '提交成功',
     UPDATE: '更新成功',
-    DELETE: '删除成功'
+    DELETE: '删除成功',
 };
 
 export const mapTitleToKeyInOrder = {
@@ -83,5 +83,5 @@ export const mapTitleToKeyInOrder = {
     '提现时间': 'updateTime',
     '提现银行卡号': 'bankCardNo',
     '收款人': 'bankName',
-    '提现状态': 'status'
+    '提现状态': 'status',
 };
